64ed6d033276d46f06e16f476edd072210f28bc5
Milos Petrovic <milos.petrovic@esome.com>
2019-11-27 16:59:56 +0100

    #4 All done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Game } from './game/Game';
 
+import './App.css';
+
 function App() {
   return (
     <div className="App">
-      <Game />
+      <div className="game-wrapper">
+        <Game />
+        <div className="frame"></div>
+      </div>
     </div>
   );
 }
