8c29f7ea94aece0d747e62cb86c79c462819ec85
Milos Petrovic <milos.petrovic@esome.com>
2019-11-27 16:53:37 +0100

    Updates

---
+++ precache-manifest.0aecc7fae5b0c0b0e02efc8e1d66f6a3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0a52bacd175326a75f5c1d51b7be713",
+    "url": "/wanted/index.html"
+  },
+  {
+    "revision": "8da5eb3aa5a0c917f94f",
+    "url": "/wanted/static/css/main.0fa8bed3.chunk.css"
+  },
+  {
+    "revision": "f228d67bcd980ef4bc2f",
+    "url": "/wanted/static/js/2.7e1a96a0.chunk.js"
+  },
+  {
+    "revision": "8da5eb3aa5a0c917f94f",
+    "url": "/wanted/static/js/main.470b6030.chunk.js"
+  },
+  {
+    "revision": "4e771d0655da81bc7bb2",
+    "url": "/wanted/static/js/runtime-main.e629d7b7.js"
+  },
+  {
+    "revision": "03c84f510b4ee0d0c94842094d36ea99",
+    "url": "/wanted/static/media/background.03c84f51.jpg"
+  },
+  {
+    "revision": "946e50a410c6f185bed1207a212ebf48",
+    "url": "/wanted/static/media/bg-color.946e50a4.svg"
+  },
+  {
+    "revision": "96fe30d94c3a3d9774c94b1b7107ae7a",
+    "url": "/wanted/static/media/bullet-holes.96fe30d9.png"
+  },
+  {
+    "revision": "0bbeff7e6499fc0522cebc4d172a9c15",
+    "url": "/wanted/static/media/frame.0bbeff7e.png"
+  },
+  {
+    "revision": "75b647c7b85286d071fdb47d065e1aec",
+    "url": "/wanted/static/media/hat.75b647c7.png"
+  },
+  {
+    "revision": "afe82882409e014f9e4693faf4d423c9",
+    "url": "/wanted/static/media/idle.afe82882.png"
+  },
+  {
+    "revision": "e850e7478b10774ab6f2da5afd517269",
+    "url": "/wanted/static/media/left.e850e747.png"
+  },
+  {
+    "revision": "44afdfdcd2cf50379598e626604d9981",
+    "url": "/wanted/static/media/lose.44afdfdc.mp3"
+  },
+  {
+    "revision": "28873990edf7cb9b9e907b90c1f92c3f",
+    "url": "/wanted/static/media/lost.28873990.png"
+  },
+  {
+    "revision": "6b68e6d1226dbf584517ef37a1a3c380",
+    "url": "/wanted/static/media/prepare.6b68e6d1.mp3"
+  },
+  {
+    "revision": "016610838a97da756a6ee07f0a3232cf",
+    "url": "/wanted/static/media/railroad-webfont.01661083.eot"
+  },
+  {
+    "revision": "426b3a10e4588d94c3f71d01cf32d7ae",
+    "url": "/wanted/static/media/railroad-webfont.426b3a10.ttf"
+  },
+  {
+    "revision": "4981270bfe8fd8cc9d3ea54e52a09ca3",
+    "url": "/wanted/static/media/railroad-webfont.4981270b.svg"
+  },
+  {
+    "revision": "9966fc96cc000d0e3e3febf58bc6f15b",
+    "url": "/wanted/static/media/railroad-webfont.9966fc96.woff"
+  },
+  {
+    "revision": "e9f103ce47cfa9855f72908952ff3608",
+    "url": "/wanted/static/media/railroad-webfont.e9f103ce.woff2"
+  },
+  {
+    "revision": "cbd2fdc815aa54664af1a2fbfdb7af48",
+    "url": "/wanted/static/media/right.cbd2fdc8.png"
+  },
+  {
+    "revision": "1278f802154252f297e58b307136cdf0",
+    "url": "/wanted/static/media/shoot.1278f802.mp3"
+  },
+  {
+    "revision": "2b8bdcbc1c0a105bd46b539e101e64c8",
+    "url": "/wanted/static/media/western.2b8bdcbc.woff"
+  },
+  {
+    "revision": "77ff500cd9440874f313ab2a6bbb4932",
+    "url": "/wanted/static/media/western.77ff500c.svg"
+  },
+  {
+    "revision": "a4eedaa09e2509d9baaffaedb684f406",
+    "url": "/wanted/static/media/western.a4eedaa0.woff2"
+  },
+  {
+    "revision": "bacd927c8b010a6a1baf273f9f88613d",
+    "url": "/wanted/static/media/western.bacd927c.ttf"
+  },
+  {
+    "revision": "e0d49af0230e3c56e160818880e735cf",
+    "url": "/wanted/static/media/western.e0d49af0.eot"
+  },
+  {
+    "revision": "5671c7a190b0bebcf301950e9d968d7f",
+    "url": "/wanted/static/media/wheel.5671c7a1.png"
+  },
+  {
+    "revision": "890438967e2cd22adc1656284a7eb2ed",
+    "url": "/wanted/static/media/win.89043896.mp3"
+  },
+  {
+    "revision": "c20cf9d4b92e0b2093e9e5e4cc7ee41d",
+    "url": "/wanted/static/media/won.c20cf9d4.png"
+  }
+]);
